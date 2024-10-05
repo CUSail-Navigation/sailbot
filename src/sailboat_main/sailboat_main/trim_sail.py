71996dc937a217fa2dbf4d586896de926ad4d92d
晨赫1491 <liziang@dhcp-vl2041-44807.eduroam.cornell.edu>
2024-10-05 12:19:33 -0400

    documentation added

--- src/sailboat_main/sailboat_main/trim_sail.py
+++ src/sailboat_main/sailboat_main/trim_sail.py
@@ -4,6 +4,10 @@ from rclpy.node import Node
 from std_msgs.msg import Int32
 
 class TrimSail(Node):
+    """
+    The sailing algorithm responsible for changing the sail angle based on the 
+    wind direction.
+    """
 
     def __init__(self):
         super().__init__('trim_sail')
@@ -14,6 +18,7 @@ class TrimSail(Node):
         #     10)
         # self.subscription  # prevent unused variable warning
         
+        #Subscription for wind direction
         self.subscription = self.create_subscription(
             Int32,
             '/wind',
@@ -21,16 +26,23 @@ class TrimSail(Node):
             10)
         self.subscription  # prevent unused variable warning
 
+        # Publisher for sail angle
         self.publisher_ = self.create_publisher(Int32, '/sail', 10)
 
     def wind_callback(self, msg):
+        """
+        Use the wind data from msg to calculate the sail angle and publish the absolute value 
+        of the sail angle
+        """
         sail_angle = abs(self.setSail(msg.data))
         self.publisher_.publish(sail_angle)
         self.get_logger().info('Sail Angle: "%s"' % sail_angle)
 
     def setSail(self, windDir):
         """
-        Calculate and set the new sail angle given ... Figure out what parameters are needed
+        Calculate and set the new sail angle given wind direction.
+        Parameter precondition: windDir, an int in [0,360)
+        Return value: the sail angle, an int in [-90,90]
         """
         # wind is blowing in the same direction as the sailing direction (run), this range
         # sets a 20 degree buffer zone so that the sail does not always flip.
