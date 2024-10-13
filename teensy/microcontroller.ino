4f6a42d04f43968ae9a02337e2c44f33dec4ab58
Sean Zhang <swz9@cornell.edu>
2024-10-13 15:57:36 -0400

    edits for testing

--- teensy/microcontroller.ino
+++ teensy/microcontroller.ino
@@ -16,10 +16,10 @@ float windAngle;
 
 void setup() {
   Serial.begin(9600); //Anem
-  SerialUSB1.begin(9600); //Sail
-  SerialUSB2.begin(9600); //Tail
-  servoTail.attach(3);
-  servoSail.attach(4);
+  Serial1.begin(9600); //Sail
+  Serial2.begin(9600); //Tail
+  servoTail.attach(4);
+  servoSail.attach(3);
   pinMode(pwmPin3, OUTPUT);
   pinMode(pwmPin4, OUTPUT);
   servoSail.write(1050);
@@ -31,17 +31,6 @@ float calculateWindAngle(int sensorValue) {
 }
 
 void loop() {
- // if (Serial.available()) {
-
-  // String inputString = Serial.readStringUntil('\n'); // Read until newline
-  // inputString.trim();
-
-  // int spaceIndex = inputString.indexOf(' ');
-
-  // String sailString = inputString.substring(0, spaceIndex);
-  //int sail = inputString.toInt(); // Convert to integer
-
-  //int sailPWM = map(sail, 0, 90, 1050, 1300);
  // Non-blocking read for first servo (from SerialUSB1)
   if (SerialUSB1.available()) {
     char incomingByte = SerialUSB1.read();  // Read one byte
@@ -63,24 +52,14 @@ void loop() {
     if (incomingByte == '\n') {
       // End of command, convert input to angle and move servo1
       int tail_angle = servoInput2.toInt();
-      servoTail.write(map(tail_angle, 0, 90, 1050, 1300));  // Move servo1
+      servoTail.write(map(tail_angle, -30, 30, 1300, 1690));  // Move servo1
       SerialUSB2.print("Tail set to PWM: ");
       SerialUSB2.println(tail_angle);
-      servoInput1 = "";  // Clear buffer after processing
+      servoInput2 = "";  // Clear buffer after processing
     } else {
-      servoInput1 += incomingByte;  // Accumulate characters
+      servoInput2 += incomingByte;  // Accumulate characters
     }
   }
-  // String tailString = inputString.substring(spaceIndex + 1);
-  
-  // int tail = tailString.toInt(); // Convert to integer
-
- // Serial.println(sail);
-  // Serial.println(tail);
-
- // servoSail.write(sailPWM);
-  // servoTail.write(tail);
   delay(50); 
-
- // }
+ }
 }
