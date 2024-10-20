ddfbea97c5e7b16dd61cdabba84562db848fcf85
Nikil-Shyamsunder <nikil.shyamsunder@gmail.com>
2024-10-19 23:43:49 -0400

    Basic webserver

--- src/sailboat_launch/launch/sailboat.launch_sim.py
+++ src/sailboat_launch/launch/sailboat.launch_sim.py
@@ -75,6 +75,18 @@ def generate_launch_description():
       parameters=[config]
    )
 
+   # Add the rosbridge_server node
+   rosbridge_node = Node(
+        package='rosbridge_server',
+        executable='rosbridge_websocket',
+        name='rosbridge_websocket',
+        output='screen',
+        parameters=[{
+            'port': 9090  # This is the default WebSocket port for rosbridge
+        }]
+    )
+
+
    ld = LaunchDescription()
 
    # Sensors
@@ -86,5 +98,6 @@ def generate_launch_description():
    ld.add_action(event_driver_cmd)
    ld.add_action(radio_cmd)
    ld.add_action(mux_cmd)
+   ld.add_action(rosbridge_node)
 
    return ld
