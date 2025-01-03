785aa33f23ff013f6729090c9a17bfd31ae1a9b1
liya-mei <liya29mei@gmail.com>
2025-01-02 20:31:19 -0700

    Updated comments

--- teensy/src/Monitors/AnemometerMonitor.cpp
+++ teensy/src/Monitors/AnemometerMonitor.cpp
@@ -1,7 +1,5 @@
 #include "AnemometerMonitor.hpp"
 
-
-
 AnemometerMonitor::AnemometerMonitor()
 {
     pinMode(constants::anemometer::ANEMOMETER_PIN, INPUT);
@@ -10,6 +8,6 @@ AnemometerMonitor::AnemometerMonitor()
 void AnemometerMonitor::execute()
 {  
     sfr::anemometer::wind_angle = ((0.97826))*((360*analogRead(constants::anemometer::ANEMOMETER_PIN))/1000);//0.97826 = 360/368
-    //used to hit 360 but no longer
-    Serial.println(sfr::anemometer::wind_angle); //print for testing
+    //adjusted value converts 368 to 360
+    //Serial.println(sfr::anemometer::wind_angle); //print for testing
 }
