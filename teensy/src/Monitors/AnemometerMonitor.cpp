1d5a27bc3df9657bcf17948774205f78bd675b57
liya-mei <liya29mei@gmail.com>
2025-01-02 20:20:04 -0700

    Updated AnemometerMonitor to update wind angle

--- teensy/src/Monitors/AnemometerMonitor.cpp
+++ teensy/src/Monitors/AnemometerMonitor.cpp
@@ -1,9 +1,15 @@
 #include "AnemometerMonitor.hpp"
 
+
+
 AnemometerMonitor::AnemometerMonitor()
 {
+    pinMode(constants::anemometer::ANEMOMETER_PIN, INPUT);
 }
 
 void AnemometerMonitor::execute()
-{
+{  
+    sfr::anemometer::wind_angle = ((0.97826))*((360*analogRead(constants::anemometer::ANEMOMETER_PIN))/1000);//0.97826 = 360/368
+    //used to hit 360 but no longer
+    Serial.println(sfr::anemometer::wind_angle); //print for testing
 }
