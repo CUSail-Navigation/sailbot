329aa8f09f4fe41dad8b4f374712ba092e308ad2
Wang Mak <makw@bxscience.edu>
2025-02-22 14:20:10 -0500

    Vectornav launch file

---
+++ src/sailboat_sensors/sailboat_sensors/vectornav/vectornav_fake.py
@@ -0,0 +1,81 @@
+import numpy as np
+import time
+
+class FakeVectorNav:
+    """
+    A fake data generator class for testing purposes.
+    This class simulates the behavior of the real VectorNav class,
+    providing random data for GPS and IMU readings.
+    """
+    def __init__(self):
+        # Initialize with a starting position and heading
+
+        self.latitude = 42.876379
+        self.longitude = -77.007822
+        self.heading = np.random.uniform(0, 360)
+        self.pitch = np.random.uniform(-90, 90)
+        self.roll = np.random.uniform(0, 360)
+
+    def readVectorNavYaw(self):
+        # Simulate heading change
+        self.heading += self.rate_of_turn
+        if self.heading >= 360:
+            self.heading -= 360
+        elif self.heading < 0:
+            self.heading += 360
+        return self.heading
+
+    def readVectorNavPitch(self):
+        # Simulate heading change
+        self.heading += self.rate_of_turn
+        if self.heading >= 360:
+            self.heading -= 360
+        elif self.heading < 0:
+            self.heading += 360
+        return self.heading
+    
+    def readVectorNavRoll(self):
+        # Simulate heading change
+        self.heading += self.rate_of_turn
+        if self.heading >= 360:
+            self.heading -= 360
+        elif self.heading < 0:
+            self.heading += 360
+        return self.heading
+    
+    def readVectorNavLatitude(self):
+        # Simulate slight latitude changes over time
+        self.latitude += np.random.uniform(-0.0001, 0.0001)
+        # Ensure the latitude stays within valid range
+        if self.latitude > 84:
+            self.latitude = 84
+        elif self.latitude < -80:
+            self.latitude = -80
+        return self.latitude
+
+    def readVectorNavLongitude(self):
+        # Simulate slight longitude changes over time
+        self.longitude += np.random.uniform(-0.0001, 0.0001)
+        # Ensure the longitude stays within valid range
+        if self.longitude > 180:
+            self.longitude = -180 + (self.longitude - 180)
+        elif self.longitude < -180:
+            self.longitude = 180 - (-180 - self.longitude)
+        return self.longitude
+
+    def simulate(self, duration=60):
+        """
+        Simulate the sensor data generation for a given duration in seconds.
+        """
+        start_time = time.time()
+        while time.time() - start_time < duration:
+            print(f"Latitude: {self.readVectorNavLatitude():.6f}, Longitude: {self.readVectorNavLongitude():.6f}, "
+                  f"Yaw: {self.readVectorNavYaw():.2f}, Pitch: {self.readVectorNavPitch()}, Roll: {self.readVectorNavRoll()}" )
+            time.sleep(1)
+
+
+# Example usage
+if __name__ == '__main__':
+    fake_vectornav = FakeVectorNav()
+    fake_vectornav.simulate(10)
+
---
+++ src/sailboat_sensors/sailboat_sensors/vectornav/vectornav_node.py
@@ -0,0 +1,110 @@
+import numpy as np
+import rclpy
+from rclpy.node import Node
+
+from sensor_msgs.msg import NavSatFix, Imu
+from geometry_msgs.msg import Quaternion
+from . import vectornav
+from . import vectornav_fake
+
+class VectorNav(Node):
+    def __init__(self):
+        super().__init__('vectornav')
+
+        # Declare timer period parameter
+        self.declare_parameter('timer_period', 0.5)
+        self.timer_period = self.get_parameter('timer_period').value
+
+        self.declare_parameter('vectornav_port', '') # FIXME: Add a better default port
+        self.airmar_port = self.get_parameter('vectornav_port').value
+
+        self.declare_parameter('use_fake_data', False)
+        self.use_fake_data = self.get_parameter('use_fake_data').value
+
+        self.publisher_gps = self.create_publisher(NavSatFix, '/gps', 10)
+        self.publisher_imu = self.create_publisher(Imu, '/imu', 10)
+
+        self.timer = self.create_timer(self.timer_period, self.gps_callback)
+
+        if self.use_fake_data:
+            self.vectornav = vectornav_fake.FakeVectorNav()
+            self.get_logger().info('Using fake data for Vectornav')
+        else:
+            self.vectornav = vectornav.SailVectorNav(self.vectornav)
+            self.get_logger().info('Launching Airmar with real data')
+
+    def getYaw(self):
+        return self.vectornav.readVectorNavYaw()
+    def getPitch(self):
+        return self.vectornav.readVectorNavPitch()
+    def getRoll(self):
+        return self.vectornav.readVectorNavRoll()
+    def getLat(self):
+        return self.vectornav.readVectorNavLatitude()
+    def getLong(self):
+        return self.vectornav.readVectorNavLongitude()
+
+    def gps_callback(self):
+        nav_sat_msg = NavSatFix()
+
+        nav_sat_msg.longitude = self.getLong()
+        nav_sat_msg.latitude = self.getLat()
+        nav_sat_msg.position_covariance_type = 0
+        self.publisher_gps.publish(nav_sat_msg)
+
+        imu_msg = Imu()
+        yaw = self.getYaw()
+        pitch = self.getPitch()
+        roll = self.getRoll()
+
+
+        quaternion = Quaternion()
+        qx, qy, qz, qw = self.euler_to_quaternion(yaw, pitch, roll)
+        quaternion.x = qx
+        quaternion.y = qy
+        quaternion.z = qz
+        quaternion.w = qw
+        imu_msg.orientation = quaternion
+
+        imu_msg.angular_velocity.z = self.airmar.readAirMarROT()
+        self.publisher_imu.publish(imu_msg)
+
+        self.get_logger().info('Publishing GPS Data: ' + 'Lat: ' + str(nav_sat_msg.latitude) + ' Long: ' + str(nav_sat_msg.longitude))
+        self.get_logger().info(f'Publishing IMU Data: Orientation = ({qx}, {qy}, {qz}, {qw}), Angular Velocity (Z) = {imu_msg.angular_velocity.z}' )
+
+
+    def euler_to_quaternion(self, yaw, pitch, roll):
+        """
+        Convert Euler angles to quaternion.
+
+        Parameters:
+        yaw (float): The yaw angle in radians.
+        pitch (float): The pitch angle in radians.
+        roll (float): The roll angle in radians.
+
+        Returns:
+        list: A list containing the quaternion [qx, qy, qz, qw].
+        """
+        qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+        qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
+        qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
+        qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+
+        return [qx, qy, qz, qw]
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    vectornav = VectorNav()
+
+    rclpy.spin(vectornav)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    vectornav.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
