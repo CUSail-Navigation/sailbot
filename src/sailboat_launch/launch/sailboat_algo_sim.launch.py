8310dcd0ef13f2b28b7e7ddbf807839357d0b485
Sean Zhang <swz9@cornell.edu>
2025-04-13 01:53:35 -0400

    Reorganize launch files and config files

--- src/sailboat_launch/launch/sailboat_algo_sim.launch.py
+++ src/sailboat_launch/launch/sailboat_algo_sim.launch.py
@@ -6,27 +6,25 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
+   """
+   This launch file is used to run the ROS 2 side of the algo simulator. Sensor 
+   inputs are received from the simulator via the rosbridge server. The 
+   trim_sail and main_algo nodes are run to compute sail and rudder angles. 
+   These computed angles are displayed on the simulator web-app.   
+   """
    config = os.path.join(
       get_package_share_directory('sailboat_launch'),
       'config',
-      'config.yaml'
+      'config_sim.yaml'
       )
-   gps_cmd = Node(
-      package='sailboat_sensors',
-      executable='airmar',
-      name='airmar',
-      namespace='sailbot',
-      parameters=[config]
-   )
-
-
+   
    mux_cmd = Node(
       package='sailboat_main',
       executable='mux',
       name='mux',
       namespace='sailbot',
       parameters=[config]
-   ) 
+   )
 
    main_algo_cmd  = Node(
       package='sailboat_main',
@@ -34,14 +32,6 @@ def generate_launch_description():
       name='main_algo',
       namespace='sailbot',
       parameters=[config]
-   )
-
-   teensy_cmd = Node(
-    package='sailboat_main',
-    executable='teensy',
-    name='teensy',
-    namespace='sailbot',
-    parameters=[config]
    )
 
    trim_sail_cmd = Node(
@@ -60,16 +50,32 @@ def generate_launch_description():
       parameters=[config]
    )
 
+   rosbridge_node = Node(
+        package='rosbridge_server',
+        executable='rosbridge_websocket',
+        name='rosbridge_websocket',
+        output='screen',
+        parameters=[{
+            'port': 9090  # This is the default WebSocket port for rosbridge
+        }]
+    )
 
 
    ld = LaunchDescription()
 
-   # Sensors
-   ld.add_action(gps_cmd)
+   # NOTE: no sensor nodes  since the algo simulator simulates the sensor inputs
+
+   # Autonomous sailing
    ld.add_action(main_algo_cmd)
    ld.add_action(trim_sail_cmd)
    ld.add_action(waypoint_service_cmd)
+
+   # The mux is necessary to publish sail and rudder angles to the common 
+   # sail_angle and rudder_angle topics. This is necessary if we ever want to
+   # run HITL with the simulator.
    ld.add_action(mux_cmd)
-   ld.add_action(teensy_cmd)
+
+   # Simulator communications
+   ld.add_action(rosbridge_node)
 
    return ld
