31e7fd5f757957a47a16db051e3468b22da8be39
nl545 <nl545@cornell.edu>
2025-04-19 15:01:34 -0400

    remove print statements

--- teensy/src/Monitors/SerialMonitor.cpp
+++ teensy/src/Monitors/SerialMonitor.cpp
@@ -8,16 +8,9 @@ SerialMonitor::SerialMonitor()
 
 void SerialMonitor::execute()
 {
-    //Serial.println("is executing");
     while (Serial.available())
     {
         uint8_t incoming_byte = Serial.read();
-        Serial.println(incoming_byte);
-        Serial.println("buffer conditionals");
-        Serial.println(buffer_index);
-        Serial.println(sizeof(sfr::serial::buffer));
-        Serial.println(packet_started);
-        Serial.println(incoming_byte == constants::serial::RX_END_FLAG);
         // only set flags if packet start byte is correct
         if (incoming_byte == constants::serial::RX_START_FLAG)
         {
@@ -37,15 +30,12 @@ void SerialMonitor::execute()
             sfr::servo::sail_angle = sfr::serial::buffer[0];
             sfr::servo::rudder_angle = sfr::serial::buffer[1];
             sfr::serial::servo_angle = sfr::serial::buffer[2];
-            Serial.println(sfr::serial::update_servos);
         }
         else if (packet_started)
         {
-            Serial.println("packet started");
             // store a data byte into the buffer
             if (buffer_index < sizeof(sfr::serial::buffer)) {
                 sfr::serial::buffer[buffer_index++] = incoming_byte;
-                Serial.println("added to buffer");
         }
             // if buffer is full, drop the packet
             else
