fix heading difference calculation, move points, change algo used in launch files

src/sailboat_main/sailboat_main/main_algo/sail_algo.py
@@ -12,8 +12,84 @@ from typing import Optional
 
 from sailboat_interface.srv import Waypoint
 from sailboat_interface.msg import AlgoDebug
-from .points import LatLongPoint, UTMPoint
+# from .points import LatLongPoint, UTMPoint
 from .states import SailState
+
+import utm
+import math
+# import numpy as np
+# from sensor_msgs.msg import NavSatFix
+
+class UTMPoint():
+    """
+    A class to represent a point in UTM coordinates.
+    """
+    easting : float
+    northing : float
+    zone_number : int
+    zone_letter : str
+
+    def __init__(self, easting, northing, zone_number, zone_letter):
+        self.easting = easting
+        self.northing = northing
+        self.zone_number = zone_number
+        self.zone_letter = zone_letter
+
+    def to_latlon(self) -> 'LatLongPoint':
+        """
+        Convert UTM coordinates to latitude and longitude.
+        """
+        latitude, longitude = utm.to_latlon(self.easting, self.northing, self.zone_number, self.zone_letter)
+        return LatLongPoint(latitude, longitude) 
+
+    def to_navsatfix_msg(self) -> NavSatFix:
+        """
+        Convert UTM coordinates to NavSatFix message.
+        """
+        lat_long = self.to_latlon()
+        msg = NavSatFix()
+        msg.latitude = lat_long.latitude
+        msg.longitude = lat_long.longitude
+        return msg
+    
+    def distance_to(self, other: 'UTMPoint') -> float:
+        """
+        Calculate the distance to another UTM point.
+        """
+        assert self.zone_number == other.zone_number, "Zone numbers must be the same for distance calculation"
+        return math.dist((self.easting, self.northing), (other.easting, other.northing))
+    
+    def target_bearing_to(self, other: 'UTMPoint') -> float:
+        """
+        Calculate the bearing to another UTM point.
+        """
+        assert self.zone_number == other.zone_number, "Zone numbers must be the same for bearing calculation"
+        delta_easting = self.easting - other.easting
+        delta_northing = self.northing - other.northing
+        return np.arctan2(delta_northing, delta_easting) * 180 / np.pi
+
+    def __repr__(self):
+        return f"UTMPoint(x={self.x}, y={self.y}, zone_number={self.zone_number}, zone_letter={self.zone_letter})"
+
+class LatLongPoint():
+    """
+    A class to represent a point in latitude and longitude coordinates.
+    """
+    latitude : float
+    longitude : float
+    def __init__(self, latitude, longitude):
+        self.latitude = latitude
+        self.longitude = longitude
+
+    def to_utm(self) -> 'UTMPoint':
+        """
+        Convert latitude and longitude to UTM coordinates.
+        """
+        x, y, zone_number, zone_letter = utm.from_latlon(self.latitude, self.longitude)
+        return UTMPoint(x, y, zone_number, zone_letter) 
+
+    def __repr__(self):
+        return f"latitude={self.latitude}, longitude={self.longitude}"
 
 class SailAlgo(Node):
     """
@@ -136,7 +212,7 @@ class SailAlgo(Node):
     
         # update heading difference: heading_direction - target_bearing
         self.heading_difference = np.mod(self.heading_direction -
-                                         self.current_location.target_bearing_to(self.current_destination) + 180, 360) - 180
+                                         self.current_destination.target_bearing_to(self.current_location) + 180, 360) - 180
         self.get_logger().info(f'Heading Difference: {self.heading_difference}')
         
         self.update_state()
@@ -496,6 +572,7 @@ class SailAlgo(Node):
         """
         Use the imu data to assign value to self.heading_direction
         """
+        self.get_logger().info(f'Heading Direction: {msg.z}')
         data = msg.z
         self.heading_direction = data
         