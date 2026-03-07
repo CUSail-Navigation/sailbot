f380965d33e14af388c1d4437c1c46d80415c3e0
nizhner <kn449@cornell.edu>
2026-03-07 14:03:31 -0500

    Set up the cpp ecosystem.

---
+++ src/cpp_files/cmake-build-cusail/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /tmp/sailbot/src/cpp_files
+# Build directory: /tmp/sailbot/src/cpp_files/cmake-build-cusail
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
