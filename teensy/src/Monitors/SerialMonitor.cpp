9b135f4d8f79dc6eff1b3a04dd77442bd2eb3690
nizhnerk <kainizhner@gmail.com>
2026-04-25 13:27:31 -0400

    Finished SerialMonitor.cpp -- some small logic fixes.

--- teensy/src/Monitors/SerialMonitor.cpp
+++ teensy/src/Monitors/SerialMonitor.cpp
@@ -12,22 +12,22 @@ void SerialMonitor::execute() {
         if (packet_started && (millis() - packet_start_time > constants::serial::RX_PACKET_TIMEOUT_MS)) drop_packet();
 
         const uint8_t new_byte = Serial.read();
-        // Reading beginning of the packet: only set flags if the packet start byte is correct.
         if (new_byte == constants::serial::RX_START_FLAG) {
             buffer_index = 0;
             packet_started = true;
             packet_start_time = millis();
         }
-        // Reading from the middle of the packet: store a data byte into the buffer.
-        else if (packet_started) {
+        else if (packet_started && new_byte != constants::serial::RX_END_FLAG) {
             if (buffer_index < sfr::serial::buffer_length) sfr::serial::buffer[buffer_index++] = new_byte;
-            else drop_packet(); // Packet is incorrect (buffer full; RX_END_FLAG not where it should be).
+            else drop_packet(); // Packet is incorrect (buffer is full and RX_END_FLAG is not where it should be).
         }
-        // Reading end of the packet: check if the packet end byte is correct and the buffer is full.
-        else if (new_byte == constants::serial::RX_END_FLAG && buffer_index == sfr::serial::buffer_length && packet_started) {
-            buffer_index = 0;
-            packet_started = false;
-            sfr::serial::update_servos = true;
+        else if (packet_started && new_byte == constants::serial::RX_END_FLAG) {
+            if (buffer_index == sfr::serial::buffer_length) {
+                buffer_index = 0;
+                packet_started = false;
+                sfr::serial::update_servos = true;
+            }
+            else drop_packet();
         }
     }
 }
