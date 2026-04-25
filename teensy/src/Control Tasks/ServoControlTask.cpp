29743fde9750c893c0baf6b68716aad4b2246bbc
nizhnerk <kainizhner@gmail.com>
2026-04-25 15:09:02 -0400

    Added BOOM_LENGTH_CM_SQUARED to constants.hpp, reworked some law of cosines stuff accordingly.

--- teensy/src/Control Tasks/ServoControlTask.cpp
+++ teensy/src/Control Tasks/ServoControlTask.cpp
@@ -4,16 +4,16 @@
  *  Construct a \code ServoControlTask\endcode and initialize all servos to default values.
  */
 ServoControlTask::ServoControlTask() {
-    mainsail_servo.attach(constants::servo::MAINSAIL_PIN, constants::servo::MAINSAIL_MIN_PULSE, constants::servo::MAINSAIL_MAX_PULSE);
     rudder_servo.attach(constants::servo::RUDDER_PIN, constants::servo::RUDDER_MIN_PULSE, constants::servo::RUDDER_MAX_PULSE);
+    mainsail_servo.attach(constants::servo::MAINSAIL_PIN, constants::servo::MAINSAIL_MIN_PULSE, constants::servo::MAINSAIL_MAX_PULSE);
     jib_port_servo.attach(constants::servo::JIB_PORT_PIN, constants::servo::JIB_PORT_MIN_PULSE, constants::servo::JIB_PORT_MAX_PULSE);
     jib_stb_servo.attach(constants::servo::JIB_STB_PIN, constants::servo::JIB_STB_MIN_PULSE, constants::servo::JIB_STB_MAX_PULSE);
 
-    // (2025-2026) Pre-set the mainsail to "all-in", and rudder to center.
-    actuate_servo(mainsail_servo, constants::servo::MAINSAIL_MIN_PULSE);
+    // (2025-2026) Pre-set the rudder to center, mainsail to "all-in", and jib to "all-in" on the port side.
     actuate_servo(rudder_servo, constants::servo::RUDDER_MID_PULSE);
+    actuate_servo(mainsail_servo, constants::servo::MAINSAIL_MIN_PULSE);
     actuate_servo(jib_port_servo, constants::servo::JIB_PORT_MIN_PULSE);
-    actuate_servo(jib_stb_servo, constants::servo::JIB_STB_MIN_PULSE);
+    actuate_servo(jib_stb_servo, constants::servo::JIB_STB_MAX_PULSE);
 }
 
 /**
@@ -28,16 +28,16 @@ void ServoControlTask::execute() {
         const uint8_t jib_stb_angle = sfr::serial::buffer[3];
 
         // Update sfr values based on incoming serial data if checks pass.
+        if (rudder_angle >= constants::servo::RUDDER_MIN_ANGLE && rudder_angle <= constants::servo::RUDDER_MAX_ANGLE) {
+            sfr::servo::rudder_angle = rudder_angle;
+            sfr::servo::rudder_pwm = rudder_to_pwm(rudder_angle);
+            actuate_servo(rudder_servo, sfr::servo::rudder_pwm);
+        }
         if (mainsail_angle >= constants::servo::MAINSAIL_MIN_ANGLE &&
             mainsail_angle <= constants::servo::MAINSAIL_MAX_ANGLE) {
             sfr::servo::mainsail_angle = mainsail_angle;
             sfr::servo::mainsail_pwm = mainsail_to_pwm(mainsail_angle);
             actuate_servo(mainsail_servo, sfr::servo::mainsail_pwm);
-        }
-        if (rudder_angle >= constants::servo::RUDDER_MIN_ANGLE && rudder_angle <= constants::servo::RUDDER_MAX_ANGLE) {
-            sfr::servo::rudder_angle = rudder_angle;
-            sfr::servo::rudder_pwm = rudder_to_pwm(rudder_angle);
-            actuate_servo(rudder_servo, sfr::servo::rudder_pwm);
         }
         // (Temporary) Store jib command bytes from serial so we can plumb data end-to-end.
         sfr::servo::jib_port_angle = jib_port_angle;
@@ -67,13 +67,12 @@ uint32_t ServoControlTask::rudder_to_pwm(uint8_t angle) {
  * @return the PWM to actuate \code mainsail_servo\endcode to.
  */
 uint32_t ServoControlTask::mainsail_to_pwm(uint8_t angle) {
-    // Use the law of cosines (get length of mainsheet relative to "all-in").
-    // In this case, we have c = sqrt(b^2 + b^2 - 2*b*b*cos(angle)) where b is the length of boom (cm).
-    uint8_t b = 1;                                                                                                      //TODO find real value
-    float mainsheet_len = sqrtf( 2 * b * b * (1 - cosf(angle * 0.017453)) ); // 0.017453 = pi/180.
+    // Use the law of cosines to get length of mainsheet relative to "all-in", where b is the length of the boom.
+    // In this case, we have mainsheet_len = sqrt(b^2 + b^2 - 2*b*b*cos(angle)) = sqrt(2b^2*(1 - cos(angle))).
+    float mainsheet_len = sqrtf( 2 * constants::servo::BOOM_LENGTH_CM_SQUARED * (1 - cosf(angle * 0.017453f)) ); // 0.017453 = pi/180.
 
     // Final PWM = (PWM_per_turn * num_turns + base_PWM) where num_turns = mainsheet_len / servo_wheel_circumference.
-    return (uint32_t) ( 229.3f * (mainsheet_len / 12.927f) + constants::servo::MAINSAIL_MIN_PULSE );                    //TODO consider adding some offsets + consider adding constants to file
+    return static_cast<uint32_t>(229.3f * (mainsheet_len / 12.927f) + constants::servo::MAINSAIL_MIN_PULSE);            //TODO consider adding some offsets + consider adding constants to file
 }
 
 /** Maps a goal sail angle for the jib, on the port side, to a \code jib_port_servo\endcode PWM.
--- teensy/src/Monitors/AnemometerMonitor.cpp
+++ teensy/src/Monitors/AnemometerMonitor.cpp
@@ -5,7 +5,7 @@ AnemometerMonitor::AnemometerMonitor() {
 }
 
 void AnemometerMonitor::execute() {
-    sfr::anemometer::wind_angle = ((0.97826))*((360*analogRead(constants::anemometer::ANEMOMETER_PIN))/1000);//0.97826 = 360/368
-    //adjusted value converts 368 to 360
-    //Serial.println(sfr::anemometer::wind_angle); //print for testing
+    // Note that 0.97826 = 360/368 -- adjusted value converts 368 to 360.
+    sfr::anemometer::wind_angle = (0.97826)*((360*analogRead(constants::anemometer::ANEMOMETER_PIN))/1000);
+    //Serial.println(sfr::anemometer::wind_angle); // Print for testing.
 }
