adfbd8c74231cb573e11bc302efe2f5969455cf0
nizhnerk <kainizhner@gmail.com>
2026-05-03 11:39:08 -0400

    Fixed AnemometerMonitor.cpp mapping scheme.

--- teensy/src/Monitors/AnemometerMonitor.cpp
+++ teensy/src/Monitors/AnemometerMonitor.cpp
@@ -5,7 +5,7 @@ AnemometerMonitor::AnemometerMonitor() {
 }
 
 void AnemometerMonitor::execute() {
-    // Note that 0.97826 = 360/368 -- adjusted value converts 368 to 360. //TODO fix whatever this is.
-    sfr::anemometer::wind_angle = (0.97826)*((360*analogRead(constants::anemometer::ANEMOMETER_PIN))/1000);
+    // Note that 0.35191 = 360/1023 -- maps analogRead() range of 0-1023 to 0-360 degrees.
+    sfr::anemometer::wind_angle = static_cast<int>(0.35191 * analogRead(constants::anemometer::ANEMOMETER_PIN));
     //Serial.println(sfr::anemometer::wind_angle); // Print for testing.
 }
