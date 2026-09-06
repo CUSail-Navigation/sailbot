f41e41412a57016fe747a77920ddace49b880748
nizhnerk <kainizhner@gmail.com>
2026-09-06 18:40:19 -0400

    Fix AnemometerMonitor.cpp wrapping issue.

--- teensy/src/Monitors/AnemometerMonitor.cpp
+++ teensy/src/Monitors/AnemometerMonitor.cpp
@@ -5,7 +5,7 @@ AnemometerMonitor::AnemometerMonitor() {
 }
 
 void AnemometerMonitor::execute() {
-    // Note that 0.35191 = 360/1023 -- maps analogRead() range of 0-1023 to 0-360 degrees.
-    sfr::anemometer::wind_angle = static_cast<int>(0.35191 * analogRead(constants::anemometer::ANEMOMETER_PIN));
+    // Note that 0.3515625 = 360/1024 -- maps the analogRead() range of 0-1023 onto 0-359 degrees.
+    sfr::anemometer::wind_angle = static_cast<int>(0.3515625 * analogRead(constants::anemometer::ANEMOMETER_PIN));
     //Serial.println(sfr::anemometer::wind_angle); // Print for testing.
 }
